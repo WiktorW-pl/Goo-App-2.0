5d9977866ac164f8a6846f495e35596ccd29b844
WiktorW-PL <modusii.90@wp.pl>
2023-01-08 20:44:28 +0100

    Add first logic to psr game

--- dist/psr.js
+++ dist/psr.js
@@ -1,2 +1,23 @@
 "use strict";
-const option = [...document.querySelectorAll(".option")];
+const option = Array.from([...document.querySelectorAll(".option")]);
+// const [rock, paper, scissors] = option
+let flag = true;
+option.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        option[index].style.border = 'red 2px solid';
+        console.log(flag);
+        // console.log(item, index)
+    });
+});
+const ai = () => {
+    const aiChoice = Math.floor(Math.random() * 3);
+    gameProps.aiPick = aiChoice;
+};
+const gameProps = {
+    playerPick: 0,
+    aiPick: 0,
+    games: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+};
--- src/psr.ts
+++ src/psr.ts
@@ -1,1 +1,25 @@
-const option : Element[]  = [...document.querySelectorAll(".option")]
+const option = Array.from([...document.querySelectorAll(".option")]) as HTMLElement[];
+// const [rock, paper, scissors] = option
+let flag : boolean = true
+    option.forEach((item, index) =>{
+        item.addEventListener('click', ()=>{ 
+            option[index].style.border = 'red 2px solid';
+            console.log(flag)
+            // console.log(item, index)
+        })
+    })
+
+
+
+const ai = () =>{
+    const aiChoice : number = Math.floor(Math.random() * 3);
+    gameProps.aiPick = aiChoice
+}
+const gameProps = {
+    playerPick: 0,
+    aiPick: 0,
+    games: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
