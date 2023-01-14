40ed56b0178cb6583ac5b497be70dc8d11476299
WiktorW-PL <modusii.90@wp.pl>
2023-01-14 23:07:45 +0100

    Add move_point function

--- dist/aim.js
+++ dist/aim.js
@@ -3,14 +3,19 @@ const game_board = document.querySelector('.app');
 const start_btn = document.querySelector('.game-panel__button');
 const start_game = () => {
     generate_point();
-    move_point();
 };
 const generate_point = () => {
-    const generated_point = document.createElement('div');
-    game_board.appendChild(generated_point);
-    generated_point.classList.add('dot');
+    const created_dot = document.createElement('div');
+    game_board.appendChild(created_dot);
+    created_dot.classList.add('dot');
+    move_point(created_dot);
 };
-const move_point = () => {
+const move_point = (created_dot) => {
+    const game_board_width = game_board.offsetWidth;
+    const game_board_left = game_board.offsetLeft;
+    const game_board_right = game_board.offsetLeft + game_board_width;
+    setInterval(() => {
+    }, 1000);
 };
 start_btn.addEventListener('click', start_game);
 //
