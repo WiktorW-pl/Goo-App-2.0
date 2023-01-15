7ee01dcd62445a61a75b313dab53e33ed46c4f58
WiktorW-PL <modusii.90@wp.pl>
2023-01-15 14:13:18 +0100

    Add the functionality of generating a point in a random place

--- dist/aim.js
+++ dist/aim.js
@@ -1,6 +1,7 @@
 "use strict";
 const game_board = document.querySelector('.app');
 const start_btn = document.querySelector('.game-panel__button');
+const dificulty_lvl = 10;
 const start_game = () => {
     generate_point();
 };
@@ -11,10 +12,17 @@ const generate_point = () => {
     move_point(created_dot);
 };
 const move_point = (created_dot) => {
-    const game_board_width = game_board.offsetWidth;
-    const game_board_left = game_board.offsetLeft;
-    const game_board_right = game_board.offsetLeft + game_board_width;
     setInterval(() => {
+        const game_board_width = game_board.offsetWidth;
+        const x_min = game_board.offsetLeft;
+        const x_max = game_board.offsetLeft + game_board_width;
+        const game_board_height = game_board.offsetHeight;
+        const y_min = game_board.offsetTop;
+        const y_max = game_board.offsetTop + game_board.offsetHeight;
+        const x = Math.floor(Math.random() * (x_max - x_min + dificulty_lvl)) + x_min;
+        var y = Math.floor(Math.random() * (y_max - y_min + dificulty_lvl)) + y_min;
+        created_dot.style.left = (x - 120) + 'px';
+        created_dot.style.top = (y - 120) + 'px';
     }, 1000);
 };
 start_btn.addEventListener('click', start_game);
--- src/aim.ts
+++ src/aim.ts
@@ -1,6 +1,6 @@
 const game_board = document.querySelector('.app') as HTMLDivElement
 const start_btn = document.querySelector('.game-panel__button') as HTMLButtonElement;
-
+const dificulty_lvl = 10;
 
 const start_game = () => {
     generate_point()
@@ -14,11 +14,17 @@ const generate_point = () => {
 }
 
 const move_point = (created_dot : HTMLElement) => {
-    const game_board_width : number = game_board.offsetWidth;
-    const game_board_left : number = game_board.offsetLeft;
-    const game_board_right : number = game_board.offsetLeft + game_board_width;
     setInterval(()=>{
-        
+
+        const x_min : number = game_board.offsetLeft;
+        const x_max : number = game_board.offsetLeft + game_board.offsetWidth;
+        const y_min : number = game_board.offsetTop;
+        const y_max : number = game_board.offsetTop + game_board.offsetHeight;
+
+        const x = Math.floor(Math.random() * (x_max - x_min + dificulty_lvl)) + x_min;
+        const y = Math.floor(Math.random() * (y_max - y_min + dificulty_lvl)) + y_min;
+        created_dot.style.left = x + 'px';
+        created_dot.style.top = y + 'px';
     }, 1000)
 }
 
