f993f7376a13b4b7d848ac14318edf0f4a09e2cd
WiktorW-PL <modusii.90@wp.pl>
2023-01-29 13:15:06 +0100

    Fixes for removeTask function

--- dist/todo.js
+++ dist/todo.js
@@ -1,1 +1,44 @@
 "use strict";
+const addTaskBtn = document.querySelector('.add-btn');
+const removeTaskBtn = document.querySelector('.remove-btn');
+const appContainer = document.querySelector('.app-todo');
+let tasksList = [];
+const buildTask = () => {
+    const taskContainer = document.createElement('div');
+    appContainer.appendChild(taskContainer);
+    taskContainer.classList.add('task');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    taskContainer.appendChild(checkbox);
+    checkbox.classList.add('task__checkbox');
+    const taskValue = document.createElement('textarea');
+    taskContainer.appendChild(taskValue);
+    taskValue.classList.add('task__input');
+    const priority = document.createElement('div');
+    taskContainer.appendChild(priority);
+    priority.classList.add('color');
+    saveTask(taskContainer, checkbox, priority);
+    setPriority(priority);
+};
+const setPriority = (priority) => {
+    priority.style.borderRight = '3px solid green';
+};
+const saveTask = (taskContainer, checkbox, priority) => {
+    const taskObj = {
+        task: taskContainer,
+        isDone: checkbox,
+        priority: priority
+    };
+    tasksList.push(taskObj);
+};
+const removeTask = () => {
+    tasksList = tasksList.filter(item => {
+        if (item.isDone.checked) {
+            item.task.remove();
+            return false;
+        }
+        return true;
+    });
+};
+addTaskBtn.addEventListener('click', buildTask);
+removeTaskBtn.addEventListener('click', removeTask);
--- src/todo.ts
+++ src/todo.ts
@@ -1,1 +1,54 @@
-console.log('test')
+const addTaskBtn = document.querySelector('.add-btn') as HTMLButtonElement
+const removeTaskBtn = document.querySelector('.remove-btn') as HTMLButtonElement
+const appContainer = document.querySelector('.app-todo') as HTMLElement
+
+
+let tasksList: any = []
+
+const buildTask = () =>{
+    const taskContainer = document.createElement('div');
+        appContainer.appendChild(taskContainer);  
+        taskContainer.classList.add('task');
+
+    const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        taskContainer.appendChild(checkbox);
+        checkbox.classList.add('task__checkbox');
+
+    const taskValue = document.createElement('textarea');
+        taskContainer.appendChild(taskValue);
+        taskValue.classList.add('task__input');
+
+    const priority = document.createElement('div');
+        taskContainer.appendChild(priority);
+        priority.classList.add('color');
+
+    saveTask(taskContainer, checkbox, priority)
+    setPriority(priority)
+}
+
+const setPriority = (priority: any) =>{
+    priority.style.borderRight = '3px solid green'
+}
+
+const saveTask = (taskContainer: HTMLDivElement, checkbox: HTMLElement, priority: HTMLElement) =>{
+    const taskObj = {
+        task: taskContainer,
+        isDone: checkbox,
+        priority: priority
+    }        
+    tasksList.push(taskObj)
+}
+
+const removeTask = () =>{
+    tasksList = tasksList.filter(item => {
+        if (item.isDone.checked) {
+          item.task.remove();
+          return false;
+        }
+        return true;
+      });
+}
+
+addTaskBtn.addEventListener('click', buildTask);
+removeTaskBtn.addEventListener('click', removeTask);
