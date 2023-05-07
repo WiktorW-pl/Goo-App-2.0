acce8e891f64a28371dd8234e6b29adfa0a402f6
WiktorW-PL <modusii.90@wp.pl>
2023-05-07 12:34:16 +0200

    Add TweenMax, and switch content animations

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// const { TweenMax } = require("gsap/gsap-core");
+
 const notesBtn = document.querySelector('#go-to-notes');
 const todoBtn = document.querySelector('#go-to-todo');
+const switchBtn = document.querySelector('.button-switch')
+const toolsBox = document.querySelector('.content')
+const gamesBox = document.querySelector('.content-games')
+const switchBtnMobile = document.querySelector('#btnSwitchHamburger')
+let currentView = 'tools'
 
 const hamburger = document.querySelector('.hamburger');
 const hamburgerNav = document.querySelector('.hamburger-navigation');
@@ -13,4 +20,44 @@ const handleClick = () =>{
       }
 }
 
+const swtichContent = () =>{
+  switchBtn.classList.add('disabled');
+  switchBtn.disabled = true;
+  setTimeout(function() {
+    switchBtn.classList.remove('disabled');
+    switchBtn.disabled = false;
+  }, 1000);
+  if(currentView === 'tools'){
+    TweenMax.to(toolsBox, 0.5, {x: -2000, display: 'none'})
+    TweenMax.fromTo(gamesBox, 0.5, {display: 'flex', display: 'none', x: 2000}, {delay: 0.5, display: 'flex', display: 'flex', x: 0})
+    currentView = 'games'
+  }
+  else if(currentView === 'games'){
+    TweenMax.to(gamesBox, 0.5, {x: -2000, display: 'none'})
+    TweenMax.fromTo(toolsBox, 0.5, {display: 'flex', display: 'none', x: 2000}, {delay: 0.5, display: 'flex', display: 'flex', x: 0})
+    currentView = 'tools'
+  }
+}
+
+const swtichContentMobile = () =>{
+  switchBtnMobile.classList.add('disabled');
+  switchBtnMobile.disabled = true;
+  setTimeout(function() {
+    switchBtnMobile.classList.remove('disabled');
+    switchBtnMobile.disabled = false;
+  }, 1000);
+  if(currentView === 'tools'){
+    TweenMax.to(toolsBox, 0.5, {y: 2000, display: 'none'})
+    TweenMax.fromTo(gamesBox, 0.5, {display: 'flex', display: 'none', y: 2000}, {delay: 0.5, display: 'flex', display: 'flex', y: 0})
+    currentView = 'games'
+  }
+  else if(currentView === 'games'){
+    TweenMax.to(gamesBox, 0.5, {y: 2000, display: 'none'})
+    TweenMax.fromTo(toolsBox, 0.5, {display: 'flex', display: 'none', y: 2000}, {delay: 0.5, display: 'flex', display: 'flex', y: 0})
+    currentView = 'tools'
+  }
+}
+
 hamburger.addEventListener('click', handleClick)
+switchBtn.addEventListener('click', swtichContent)
+switchBtnMobile.addEventListener('click', swtichContentMobile)
